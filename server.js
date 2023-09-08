7b0a683e703eb1ee4628fc703b580e8d7a6c5234
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-09-08 07:41:13 -0400

    The endpoint returns user info

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+	const { slack_name, track } = req.query;
+
+	if (!slack_name || track) {
+		return res.status(400).json({ message: "slack_name and track are required query parameters" })
+	}
+
+	const currentDate = new Date();
+	const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	const currentDay = dayNames[currentDate.getUTCDate()];
+	const utcTime = currentDate.toISOString();
+
+	return res.status(200).json({
+		slack_name,
+		current_day: currentDay,
+		utc_time: utcTime,
+		track,
+		github_file_url: "https://github.com/Abiodun-Shittu/backend-stage-one/main/server.js",
+		github_repo_url: "https://github.com/Abiodun-Shittu/backend-stage-one",
+		status_code: 200
+	})
+})
+
+app.listen(port, () => {
+	console.log(`server listening on http://localhost:${port}`);
+});
