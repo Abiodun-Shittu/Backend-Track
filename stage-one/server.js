e0af182015d58bb3e3d487abe2c2405491379436
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-09-08 20:01:54 -0400

    Add test endpoint

--- stage-one/server.js
+++ stage-one/server.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	return res.status(200).json({ message: "This is a test route"})
+})
+
 app.get("/api", (req, res) => {
 	const { slack_name, track } = req.query;
 
